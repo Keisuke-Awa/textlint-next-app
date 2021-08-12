0c75504371562e3aa9b104fa96dbb6ebf6d3e80d
Keisuke-Awa <keisuke.awa.basket@gmail.com>
2021-08-12 19:15:07 +0900

    engineの初期化を変更

--- lib/textlint/textlint.ts
+++ lib/textlint/textlint.ts
@@ -1,16 +1,13 @@
-// import { TextLintEngine } from "textlint";
+import { TextLintEngine } from "textlint";
 import path from "path";
 
-const TextLintEngine = require("textlint").TextLintEngine;
-
-const options = {
-  configFile: path.resolve("lib/textlint/.textlintrc"),
-  rulesBaseDirectory: path.resolve("node_modules"),
-};
-
-const engine = new TextLintEngine(options);
-
 const proofreadText = async (text: string) => {
+  const options = {
+    configFile: path.resolve("lib/textlint/.textlintrc"),
+    rulesBaseDirectory: path.resolve("node_modules"),
+  };
+
+  const engine = new TextLintEngine(options);
   const results = await engine.executeOnText(text);
   return results[0].messages;
 };
