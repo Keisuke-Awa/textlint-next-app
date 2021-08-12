2aab888d60cea5f1b35746bbff633491fa243e13
Keisuke-Awa <keisuke.awa.basket@gmail.com>
2021-08-12 16:11:46 +0900

    rulepathsを設定

--- lib/textlint/textlint.ts
+++ lib/textlint/textlint.ts
@@ -3,11 +3,16 @@ import path from "path";
 
 const options = {
   configFile: path.resolve("lib/textlint/.textlintrc"),
+  rulePaths: [
+    "node_modules/textlint-rule-no-double-negative-ja",
+    "node_modules/textlint-rule-prh",
+  ],
 };
 
 const engine = new TextLintEngine(options);
 
 const proofreadText = async (text: string) => {
+  console.log(engine);
   const results = await engine.executeOnText(text);
   return results[0].messages;
 };
