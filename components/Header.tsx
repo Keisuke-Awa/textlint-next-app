4bcbeefd047545e504e6051f5f2b9e9efa5d953a
Keisuke-Awa <keisuke.awa.basket@gmail.com>
2021-08-18 18:12:08 +0900

    バージョン変更

--- components/Header.tsx
+++ components/Header.tsx
@@ -19,7 +19,7 @@ const Header = (): ReactElement => {
             α
           </Tag>
           <Tag variant="outline" colorScheme="white">
-            v 0.1.9
+            v 0.2.0
           </Tag>
         </Heading>
       </Box>
